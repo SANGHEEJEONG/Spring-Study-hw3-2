a5e758f30dd80a060f99d114383adec174ffd691
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-10-07 17:26:50 +0900

    init

---
+++ src/main/java/com/example/springhw32/SpringHw32Application.java
@@ -0,0 +1,13 @@
+package com.example.springhw32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHw32Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringHw32Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springhw32/SpringHw32ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhw32;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw32ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
